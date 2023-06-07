31814c28849d1b6d0675930ef0b38523b22fd8cd
Andre Iglesias <ciglesia@student.42.fr>
2023-06-07 13:30:56 +0200

    cce_param

--- client/blindbox/tests/session.py
+++ client/blindbox/tests/session.py
@@ -3,7 +3,7 @@ from blindbox.requests  import SecureSession, requests
 SERVER_URL = "localhost"
 SERVER_PORT = 8080
 
-with SecureSession(f"http://{SERVER_URL}:{SERVER_PORT}", "./cce_policy.txt", "sampleAttester.eus.attest.azure.net", True) as secure_session:
+with SecureSession(f"http://{SERVER_URL}:{SERVER_PORT}", "./cce_policy.txt", "sampleAttester.eus.attest.azure.net") as secure_session:
 
     res = secure_session.post(endpoint="/enclave/predict", files={"audio": open("test.wav", "rb")})
     print(res.status_code)
@@ -14,10 +14,10 @@ with SecureSession(f"http://{SERVER_URL}:{SERVER_PORT}", "./cce_policy.txt", "sa
     print(res.text)
 
 
-res = requests.post(url=f"http://{SERVER_URL}:{SERVER_PORT}/enclave/predict", files={"audio": open("test.wav", "rb")})
+res = requests.post(url=f"http://{SERVER_URL}:{SERVER_PORT}/enclave/predict", "./cce_policy.txt", files={"audio": open("test.wav", "rb")})
 print(res.status_code)
 print(res.text)
 
-res = requests.get(url=f"http://{SERVER_URL}:{SERVER_PORT}/enclave")
+res = requests.get(url=f"http://{SERVER_URL}:{SERVER_PORT}/enclave", "./cce_policy.txt")
 print(res.status_code)
 print(res.text)
